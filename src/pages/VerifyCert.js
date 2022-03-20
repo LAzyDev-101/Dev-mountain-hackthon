7c394be7203fc939db1475a5532b6ebc826008bc
pisichi <win.pisichi@gmail.com>
2022-03-20 23:35:57 +0700

    update ui

--- src/pages/VerifyCert.js
+++ src/pages/VerifyCert.js
@@ -9,62 +9,76 @@ import { ReactComponent as Logo } from "../assets/logo.svg";
 import { hashEduJson } from "../utils/jspdf";
 import React, { useState } from "react";
 import { Buffer } from "buffer";
-import { PDFDocument } from 'pdf-lib'
+import { PDFDocument } from "pdf-lib";
 import { hashSha256 } from "utils/hash";
 import { useVerifyTranscript } from "hook/useEduProof";
-
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const VerifyCert = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [payload, setPayload] = useState({ eiAddress: "", studentID: "" });
   const [transData, setTransData] = useState(null);
   const [ownerData, setOwnerData] = useState(null);
-  const verifyTranscript = useVerifyTranscript()
+  const [isLoading, setIsLoading] = useState(false);
+  const verifyTranscript = useVerifyTranscript();
+
+  const loadFinish = () => {
+    setIsLoading(false);
+  };
+  const loadToggle = () => {
+    setIsLoading(true);
+  };
 
   const changeHandler = async (event) => {
-    const file = event.target.files[0]
+    const file = event.target.files[0];
     setSelectedFile(file);
     async function readPdfMetadata() {
       // Fetch PDF
 
       var reader = new FileReader();
-      const file = event.target.files[0]
+      const file = event.target.files[0];
       reader.readAsArrayBuffer(file);
       reader.onloadend = async (evt) => {
         if (evt.target.readyState === FileReader.DONE) {
           const arrayBuffer = evt.target.result,
             array = new Uint8Array(arrayBuffer);
           const pdfDoc = await PDFDocument.load(arrayBuffer, {
-            updateMetadata: false
-          })
+            updateMetadata: false,
+          });
           const author = pdfDoc.getAuthor().split(":");
           const subject = pdfDoc.getSubject().split(":");
           setPayload({
             eiAddress: author[1],
             studentID: subject[1],
-          })
-          setOwnerData(subject[0])
+          });
+          setOwnerData(subject[0]);
         }
         // Load the PDF document without updating its existing metadata
       };
     }
-    await readPdfMetadata()
-  }
+    await readPdfMetadata();
+  };
   const handdleSubmit = () => {
+    // loadToggle();
+    console.log("start", isLoading);
+
     var reader = new FileReader();
 
     reader.readAsDataURL(selectedFile);
     reader.onloadend = function () {
       var base64data = reader.result;
       setTransData(base64data.toString());
-    }
+    };
     if (transData != null) {
       // console.log(transData)
-      const hash = hashSha256(transData)
+      const hash = hashSha256(transData);
       console.log(`${payload.eiAddress} ${payload.studentID} hash: ${hash}`);
       verifyTranscript(payload.eiAddress, payload.studentID, hash).then((v) => {
-        console.log(v)
-      })
+        console.log(v);
+        loadFinish();
+        console.log("stop", isLoading);
+      });
     }
   };
 
@@ -75,9 +89,7 @@ const VerifyCert = () => {
       </div>
 
       <div className="flex flex-row justify-center items-center">
-        <div className="font-bold text-4xl font-mono">
-          ระบบขอข้อมูลผลการเรียนออนไลน์
-        </div>
+        <div className="font-bold text-4xl font-mono">ระบบขอข้อมูลผลการเรียนออนไลน์</div>
       </div>
 
       <div className="flex flex-row justify-center items-center mt-4">
@@ -92,38 +104,39 @@ const VerifyCert = () => {
 
       <div className="flex flex-row justify-center items-center mt-3">
         <div className=" basis-4/12">
-          <label class="w-full flex flex-col items-center py-1  bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue  hover:text-gray-300">
-            <input
-              type="file"
-              name="file"
-              onChange={changeHandler}
-              class="hidden"
-            />
+          <label className="w-full flex flex-col items-center py-1 mb-3  bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue  hover:text-gray-300">
+            <input type="file" name="file" onChange={changeHandler} className="hidden" />
             {selectedFile ? (
               <div>
                 <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
+                {/* <p>Filetype: {selectedFile.type}</p> */}
               </div>
             ) : (
-              <span class="text-base leading-normal">
-                เลือกไฟล์เอกสารของคุณ
-              </span>
+              <span className="text-base leading-normal">เลือกไฟล์เอกสารของคุณ</span>
             )}
           </label>
+          <span>
+            {ownerData != null && (
+              <>
+                <span className="font-bold"> Owner: </span> <span>{ownerData}</span>{" "}
+              </>
+            )}
+          </span>
         </div>
       </div>
 
-      <div className="flex flex-row justify-center items-center mt-10">
+      <div className="flex flex-row justify-center items-center mt-5">
         <button
-          class="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-3"
+          className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-3"
           onClick={handdleSubmit}
         >
           ยืนยัน
         </button>
       </div>
 
-      {ownerData!=null && 
-      <h2>{ownerData}</h2>}
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={isLoading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
   );
 };
