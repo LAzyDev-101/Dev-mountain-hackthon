4e272f41379ec35497fbd9bb102c6a2c77a1271a
khajohnyos.b <khajohnyos@blockfint.com>
2022-03-20 22:00:29 +0700

    fix hash function

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers'
 
 export const hashSha256 = (obj) => {
-    const byte32Data = ethers.utils.toUtf8Bytes(JSON.stringify(obj));
+    const byte32Data = ethers.utils.toUtf8Bytes(obj);
     const hash = ethers.utils.sha256(byte32Data);
     return hash
 };
